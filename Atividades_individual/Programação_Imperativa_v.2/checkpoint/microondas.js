8c8db53b2e1ad06b8705b72622cfec60e3a47526
Claúdio Ribeiro Junior <cr.ribeiro.jr@gmail.com>
2022-11-07 20:42:38 -0300

    feat

--- Atividades_individual/Programação_Imperativa_v.2/checkpoint/microondas.js
+++ Atividades_individual/Programação_Imperativa_v.2/checkpoint/microondas.js
@@ -1,3 +1,34 @@
+/*
+
+Olá, você foi contratado para executar este projeto. É importante que você analise, entenda o pedido do cliente e desenvolva conforme solicitado. Veja abaixo os requisitos do projeto:
+
+OBS: Entrega até 16/11/2022 -> 23:59
+
+- Precisamos desenvolver um menu para um microondas super veloz, onde teremos 5 opções de comida com seus respectivos tempos pré-definidos. 
+
+      1 - Pipoca – 10 segundos (padrão);
+      2 - Macarrão – 8 segundos (padrão);
+      3 - Carne – 15 segundos (padrão);
+      4 - Feijão – 12 segundos (padrão);
+      5 - Brigadeiro – 8 segundos (padrão); 
+
+- O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
+- Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
+- Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
+- Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
+- No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!".
+
+Sala - 8
+
+Juliett Garcia
+Alanna Mercia
+Akira Yamada
+Agnaldo Silva
+Claudio Ribeiro
+Railana
+
+*/
+
 function microondas (opcao, tempo){
     pipocaTempo = 10;
     macarraoTempo = 8;
@@ -49,4 +80,4 @@ function microondas (opcao, tempo){
     }
 }
 
-microondas (2, 8)
+microondas (9, 5)
