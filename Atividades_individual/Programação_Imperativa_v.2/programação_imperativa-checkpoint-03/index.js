b13a8b74a3cf39a796d3a04e0f0d92dc04fd46ba
Claúdio Ribeiro Junior <cr.ribeiro.jr@gmail.com>
2022-12-08 00:19:36 -0300

    feat

---
+++ Atividades_individual/Programação_Imperativa_v.2/programação_imperativa-checkpoint-03/index.js
@@ -0,0 +1,62 @@
+class Aluno{
+    constructor (nome, qtdFaltas, notas){
+        this.nome = nome;
+        this.qtdFaltas = qtdFaltas;
+        this.notas = notas;
+    }
+    calcularMedia(){
+        let media = 0;
+        for (let nota of this.notas){
+            media += nota;
+        }
+        return (media / this.notas.length).toFixed(1);
+    }
+    faltas(){
+        this.qtdFaltas ++;
+    }
+}
+
+const curso = {
+    nomeCurso:'Engenharia da Computação',
+    notaAprovacao: 70,
+    faltasMaximas: 10,
+    listaEstudantes: [],
+    addAluno(nome, qtdFaltas, notas){
+        const aluno = new Aluno(nome, qtdFaltas, notas);
+        this.listaEstudantes.push(aluno);
+    },
+    resultadoAluno(nomeAluno){
+        for (let aluno of this.listaEstudantes){
+            if(aluno.nome == nomeAluno){
+                if(aluno.calcularMedia() >= this.notaAprovacao && aluno.qtdFaltas < this.faltasMaximas){
+                    return true;
+                }else if(aluno.calcularMedia() >= this.notaAprovacao && aluno.qtdFaltas == this.faltasMaximas){
+                    if(aluno.calcularMedia() > (this.notaAprovacao+(this.notaAprovacao*0.1))){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }else {
+                    return false;
+                }
+            }
+        }
+    },
+    resultados(){
+        const alunosResultados = [];
+        for (let aluno of this.listaEstudantes){
+            alunosResultados.push(this.resultadoAluno(aluno.nome));
+        }
+        return alunosResultados;
+    }
+}
+
+curso.addAluno("Claúdio Ribeiro Junior", 5, [100, 80, 90]);
+curso.addAluno("João Santos Ferreira", 10, [70, 80, 70]);
+curso.addAluno("Adriana Ferreira", 7, [90, 80, 70]);
+
+console.log(curso.resultadoAluno("Claúdio Ribeiro Junior"));
+console.log(curso.resultadoAluno("João Santos Ferreira"));
+console.log(curso.resultadoAluno("Adriana Ferreira"));
+
+console.log(curso.resultados());
